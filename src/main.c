fedbc3eed8ca381ea625638d62f403c72eab3105
LakshmiChaitanya2008 <lakshmichaitanyachalamalasetti@gmail.com>
2026-06-12 18:51:18 +0530

    added game over

--- src/main.c
+++ src/main.c
@@ -28,6 +28,13 @@ typedef enum {
     Cell_X
 } Cell;
 
+typedef struct {
+    bool active;
+    Cell winner;
+    Vector2 line_start;
+    Vector2 line_end;
+} Game_Over;
+
 static bool game_over(Cell grid[NUM_CELLS], Cell player) {
     const int w_cases[8][3] = {
         {0, 4, 8}, {2, 4, 6}, 
@@ -144,6 +151,57 @@ static void centered_text_render(Centered_Text *ct) {
     DrawText(ct->text, ct->_x, ct->y, ct->font_size, ct->color);
 }
 
+static void grid_game_over_lines(Cell grid[NUM_CELLS], Cell player, int screen_size, Vector2 *line_start, Vector2 *line_end) {
+    const int square_size = screen_size / 3;
+
+    if(grid[0] == player && grid[4] == player && grid[8] == player) {
+        line_start -> x = 0;
+        line_start -> y = 0;
+        line_end -> x = screen_size;
+        line_end -> y = screen_size;
+
+        return;
+    }
+
+    if(grid[2] == player && grid[4] == player && grid[6] == player) {
+        line_start -> x = screen_size;
+        line_start -> y = 0;
+        line_end -> x = 0;
+        line_end -> y = screen_size;
+
+        return;
+    }
+
+     for (int row = 0; row < 3; row++)
+    {
+        int base = row * 3;
+        if (grid[base] == player && grid[base + 1] == player &&
+            grid[base + 2] == player)
+        {
+            line_start->x = 0;
+            line_start->y = row * square_size + square_size / 2;
+            line_end->x = screen_size;
+            line_end->y = row * square_size + square_size / 2;
+            return;
+        }
+    }
+
+    for (int col = 0; col < 3; col++)
+    {
+        if (grid[col] == player && grid[col + 3] == player &&
+            grid[col + 6] == player)
+        {
+            line_start->x = col * square_size + square_size / 2;
+            line_start->y = 0;
+            line_end->x = col * square_size + square_size / 2;
+            line_end->y = screen_size;
+            return;
+        }
+    }
+}
+
+
+
 int main(void)
 {
     const int screen_width = 800;
@@ -154,6 +212,13 @@ int main(void)
     Rectangle menu_start_button;
     Color menu_start_button_color = WHITE;
     Scene scene = Scene_Menu;
+
+    Game_Over game_over_state = {0};
+
+    Centered_Text game_over_text;
+    Rectangle game_over_button;
+    Centered_Text game_over_button_text;
+    Color game_over_button_color = WHITE;
 
     InitWindow(screen_width, screen_height, "Raylib Template");
     SetTargetFPS(60);
@@ -168,6 +233,19 @@ int main(void)
     centered_text_init(&menu_start_button_text, "Play", 40, menu_start_button.y + (menu_start_button.height / 4), BLACK, menu_start_button.width);
     menu_start_button_text._x += menu_start_button.x;
 
+    game_over_button.width = 200;
+    game_over_button.height = 100;
+    game_over_button.x = (screen_width - game_over_button.width) / 2;
+    game_over_button.y = screen_height - 180;
+    centered_text_init(
+        &game_over_button_text,
+        "Menu",
+        40,
+        game_over_button.y + (game_over_button.height / 4),
+        BLACK,
+        game_over_button.width
+    );
+    game_over_button_text._x += game_over_button.x;
 
     Cell grid[NUM_CELLS] = {0};
     Cell player_turn;
@@ -197,13 +275,18 @@ int main(void)
 
     while (!WindowShouldClose()) {
         Vector2 mouse_pos = GetMousePosition();
+        const int m_x = (int) floorf(mouse_pos.x / (float) square_size);
+        const int m_y = (int) floorf(mouse_pos.y / (float) square_size);
+        const int m_i  = m_y * 3 + m_x;
         switch(scene) {
             case Scene_Menu: {
                 Vector2 mouse_pos = GetMousePosition();
-
                 if(CheckCollisionPointRec(mouse_pos, menu_start_button)) {
                     if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                        game_over_state.active = false;
+                        game_over_state.winner = Cell_Empty;
                         scene = Scene_Game;
+                        
                         for (int i = 0; i < NUM_CELLS; i++) {
                             grid[i] = Cell_Empty;
                         }
@@ -223,29 +306,77 @@ int main(void)
             }
 
             case Scene_Game: {
-                 if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-                    
-                    int x = (int) floorf(mouse_pos.x / (float) square_size);
-                    int y = (int) floorf(mouse_pos.y / (float) square_size);
-                    int i  = y * 3 + x;
 
-                    if(grid[i] == Cell_Empty) {
-                        grid[y * 3 + x] = player_turn;
+    if(!game_over_state.active &&
+       IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
 
-                        if (game_over(grid, player_turn) || grid_is_full(grid)) {
-                            scene = Scene_Menu;
-                        } else {
-                            grid_update(grid, Cell_O);
+        if(m_x >= 0 && m_x < 3 &&
+           m_y >= 0 && m_y < 3 &&
+           grid[m_i] == Cell_Empty) {
 
-                            if(game_over(grid, player_turn) || grid_is_full(grid)) {
-                                scene = Scene_Menu;
-                            }
-                        }
-                    }
+            grid[m_i] = player_turn;
+
+            if(game_over(grid, player_turn)) {
+
+                game_over_state.active = true;
+                game_over_state.winner = player_turn;
+
+                grid_game_over_lines(
+                    grid,
+                    player_turn,
+                    screen_width,
+                    &game_over_state.line_start,
+                    &game_over_state.line_end
+                );
+
+            } else if(grid_is_full(grid)) {
+
+                game_over_state.active = true;
+                game_over_state.winner = Cell_Empty;
+
+            } else {
+
+                grid_update(grid, Cell_O);
+
+                if(game_over(grid, Cell_O)) {
+
+                    game_over_state.active = true;
+                    game_over_state.winner = Cell_O;
+
+                    grid_game_over_lines(
+                        grid,
+                        Cell_O,
+                        screen_width,
+                        &game_over_state.line_start,
+                        &game_over_state.line_end
+                    );
+
+                } else if(grid_is_full(grid)) {
+
+                    game_over_state.active = true;
+                    game_over_state.winner = Cell_Empty;
                 }
-
-                break;
             }
+        }
+    }
+
+   if(game_over_state.active) {
+
+    if(CheckCollisionPointRec(mouse_pos, game_over_button)) {
+
+        game_over_button_color = GREEN;
+
+        if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+            scene = Scene_Menu;
+        }
+
+    } else {
+        game_over_button_color = WHITE;
+    }
+}
+
+    break;
+}
 
             default: {
                 assert(false);
@@ -272,7 +403,19 @@ int main(void)
                 DrawLine(0, screen_height / 3, screen_width, screen_height / 3, WHITE);
                 DrawLine(0, 2 * screen_height / 3, screen_width, 2 * screen_height / 3, WHITE);
 
+               if(!game_over_state.active &&
+   m_x >= 0 && m_x < 3 &&
+   m_y >= 0 && m_y < 3 &&
+   grid[m_i] == Cell_Empty) {
 
+    DrawRectangle(
+        m_x * square_size,
+        m_y * square_size,
+        square_size,
+        square_size,
+        DARKBLUE
+    );
+}
                 int x = 0, y = 0;
 
                 for(int i = 0; i < NUM_CELLS; i++, x++) {
@@ -301,6 +444,52 @@ int main(void)
                             break;
                     }
                 }
+
+                if(game_over_state.active) {
+
+    if(game_over_state.winner != Cell_Empty) {
+        DrawLineEx(
+            game_over_state.line_start,
+            game_over_state.line_end,
+            10,
+            RED
+        );
+    }
+
+    if(game_over_state.winner == Cell_X) {
+        centered_text_init(
+            &game_over_text,
+            "You Win!",
+            60,
+            20,
+            GREEN,
+            screen_width
+        );
+    } else if(game_over_state.winner == Cell_O) {
+        centered_text_init(
+            &game_over_text,
+            "Computer Wins!",
+            60,
+            20,
+            RED,
+            screen_width
+        );
+    } else {
+        centered_text_init(
+            &game_over_text,
+            "Draw!",
+            60,
+            20,
+            YELLOW,
+            screen_width
+        );
+    }
+
+    centered_text_render(&game_over_text);
+
+    DrawRectangleRec(game_over_button, game_over_button_color);
+    centered_text_render(&game_over_button_text);
+}
                 break;
             }
 
